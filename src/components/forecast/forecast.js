b67e0680208eda2b4e76ce111002563b995a15d3
merveyyildiz <merveyildizzz41@gmail.com>
2022-07-22 12:52:57 +0300

    added alt attribute

--- src/components/forecast/forecast.js
+++ src/components/forecast/forecast.js
@@ -12,7 +12,7 @@ const Forecast = ({ data }) => {
                 return (
                     <div className="container" key={id}>
                         <div className="center">
-                            <img src={`icons/${item.weather[0].icon}.png`} className="icon" />
+                            <img src={`icons/${item.weather[0].icon}.png`} className="icon" alt="weather"/>
                         </div>
                         <p className="day">{days[id]}</p>
                         <p className="tempature">{Math.round(item.main.temp)}°C</p>
